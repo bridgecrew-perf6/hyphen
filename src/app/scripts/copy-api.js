08b0d97d85881ca45ca3d35611cabef2295b8b96
EnderDev <42723993+EnderDev@users.noreply.github.com>
2022-04-14 21:59:42 +0100

    🎨 Improve structure of project

--- src/app/scripts/copy-api.js
+++ src/app/scripts/copy-api.js
@@ -2,14 +2,18 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const { symlinkSync } = require("fs");
+const { cpSync } = require("fs");
 const { resolve } = require("path");
 const rimraf = require("rimraf");
 
 const main = async () => {
 	rimraf.sync(resolve(process.cwd(), "pages", "api"));
 
-	symlinkSync("../api", "./pages/api");
+	cpSync(
+		resolve(process.cwd(), "..", "api"),
+		resolve(process.cwd(), "pages", "api"),
+		{ recursive: true }
+	);
 };
 
 main();
