d246e8be5a4f360a640666a8e4340cf760bc9926
EnderDev <42723993+EnderDev@users.noreply.github.com>
2022-04-15 22:43:13 +0100

    💬 Redirect American spelling of artefact to British spelling

--- src/app/next.config.js
+++ src/app/next.config.js
@@ -1,6 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+
+	redirects: [
+		{
+			source: "/api/v1/artifacts/:path*",
+			destination: "/api/v1/artefacts/:path*",
+			permanent: false,
+		},
+	],
+
+	async headers() {
+		return [
+			{
+				source: "/:path*",
+				headers: [
+					{
+						key: "Server",
+						value: "Hyphen",
+					},
+				],
+			},
+		];
+	},
 };
 
 module.exports = nextConfig;
